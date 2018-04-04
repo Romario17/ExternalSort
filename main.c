38b9b72127ba9909f59f758943c54c6ee6e10578
Romario <romariofernando@outlook.com>
2018-04-04 15:39:44 -0300

    incomplete

--- main.c
+++ main.c
@@ -4,12 +4,12 @@
 #include <math.h>
 #include <time.h>
 
-#define FILE_SIZE 22
+#define FILE_SIZE 59
 #define MAX_ID 10000
 #define PATH "StudentFile.dat"
 
-void externalSort(const char *fileName, unsigned fileSize, size_t sizeOfElements,
-        int (*compare)(const void*, const void*), unsigned internalMemorySize, unsigned pathsNumber);
+void externalSort(const char *fileName, int fileSize, size_t sizeOfElements,
+        int (*compare)(const void*, const void*), int internalMemorySize, int pathsNumber);
 void fillFile();
 void readFile();
 
@@ -24,11 +24,11 @@ int compare(const void *a, const void *b) {
 
 int main(int argc, char** argv) {
     //Capacidade da memória interna (M registros)
-    unsigned short internalMemorySize;
+    int internalMemorySize;
     //Tamanho do arquivo (N registros)
-    unsigned short fileSize = (unsigned short) FILE_SIZE;
+    int fileSize = (int) FILE_SIZE;
     //Número de discos utilizados em cada passada
-    unsigned short pathsNumber;
+    int pathsNumber;
 
     printf("Capacidade da memoria interna(M registros): ");
     scanf("%hu", &internalMemorySize);
@@ -40,24 +40,25 @@ int main(int argc, char** argv) {
     readFile();
 
     externalSort(PATH, fileSize, sizeof (int), compare, internalMemorySize, pathsNumber);
-
+    
+    printf("\n");
     readFile();
 
     return (EXIT_SUCCESS);
 }
 
 void createOrderedBlocks(FILE *inputDisk, FILE **outputDisks, size_t sizeOfElements,
-        int (*compare)(const void*, const void*), unsigned internalMemorySize, unsigned pathsNumber) {
+        int (*compare)(const void*, const void*), int internalMemorySize, int pathsNumber) {
 
     void* recordsVector = malloc(sizeOfElements * internalMemorySize);
-    unsigned recordsRead = 0;
-    unsigned selectedDisk = 0;
+    int recordsRead = 0;
+    int selectedDisk = 0;
 
     while (fread(recordsVector + (sizeOfElements * recordsRead), sizeOfElements, 1, inputDisk) == 1) {
         recordsRead++;
         if (recordsRead == internalMemorySize) {
             qsort(recordsVector, recordsRead, sizeOfElements, compare);
-            for (unsigned i = 0; i < recordsRead; i++) {
+            for (int i = 0; i < recordsRead; i++) {
                 fwrite(recordsVector + (sizeOfElements * i), sizeOfElements, 1, outputDisks[selectedDisk]);
             }
             if (selectedDisk == (pathsNumber - 1))
@@ -70,7 +71,7 @@ void createOrderedBlocks(FILE *inputDisk, FILE **outputDisks, size_t sizeOfEleme
 
     if (recordsRead > 0) {
         qsort(recordsVector, recordsRead, sizeOfElements, compare);
-        for (unsigned i = 0; i < recordsRead; i++) {
+        for (int i = 0; i < recordsRead; i++) {
             fwrite(recordsVector + (sizeOfElements * i), sizeOfElements, 1, outputDisks[selectedDisk]);
         }
     }
@@ -80,18 +81,18 @@ void createOrderedBlocks(FILE *inputDisk, FILE **outputDisks, size_t sizeOfEleme
 //======================================================================================
 
 typedef struct readingDisk {
-    unsigned index;
+    int index;
     void* lastRecordAddr;
 } ReadingDisk;
 
 typedef struct heap {
     ReadingDisk* vector;
     int (*compare)(const void*, const void*);
-    unsigned size;
-    unsigned max;
+    int size;
+    int max;
 } Heap;
 
-Heap* heap_create(int (*compare)(const void*, const void*), unsigned maxElements) {
+Heap* heap_create(int (*compare)(const void*, const void*), int maxElements) {
     Heap *heap = (Heap*) malloc(sizeof (Heap));
     heap->vector = (ReadingDisk*) malloc(sizeof (ReadingDisk) * maxElements);
     heap->compare = compare;
@@ -100,8 +101,8 @@ Heap* heap_create(int (*compare)(const void*, const void*), unsigned maxElements
     return heap;
 }
 
-void heap_moveUp(Heap* heap, unsigned nodeIndex) {
-    unsigned fatherIndex = (nodeIndex - 1) / 2;
+void heap_moveUp(Heap* heap, int nodeIndex) {
+    int fatherIndex = (nodeIndex - 1) / 2;
 
     if (nodeIndex > 0) {
         if (heap->compare(heap->vector[nodeIndex].lastRecordAddr,
@@ -116,8 +117,8 @@ void heap_moveUp(Heap* heap, unsigned nodeIndex) {
     }
 }
 
-void heap_moveDown(Heap *heap, unsigned nodeIndex) {
-    unsigned childIndex = (2 * nodeIndex) + 1;
+void heap_moveDown(Heap *heap, int nodeIndex) {
+    int childIndex = (2 * nodeIndex) + 1;
 
     if (childIndex < heap->size) {
         if (childIndex < heap->size - 1) {
@@ -163,20 +164,20 @@ void heap_destroy(Heap* heap) {
 //======================================================================================
 
 void mergeOrderedBlocks(FILE **inputDisks, FILE *outputDisk, size_t sizeOfElements,
-        int (*compare)(const void*, const void*), unsigned internalMemorySize, unsigned pathsNumber, unsigned blockSize) {
+        int (*compare)(const void*, const void*), int internalMemorySize, int pathsNumber, int blockSize) {
 
     void* recordsRead = malloc(sizeOfElements * internalMemorySize);
-    unsigned* numRecordsRead = (unsigned*) calloc(internalMemorySize, sizeof (unsigned));
+    int* numRecordsRead = (int*) calloc(internalMemorySize, sizeof (int));
 
     ReadingDisk rd;
     Heap* heap = heap_create(compare, internalMemorySize);
 
-    unsigned diskIndex = 0;
+    int diskIndex = 0;
     rd.index = diskIndex;
     rd.lastRecordAddr = recordsRead;
 
     //heap config
-    for (unsigned i = 0; i < internalMemorySize &&
+    for (int i = 0; i < internalMemorySize &&
             fread(rd.lastRecordAddr, sizeOfElements, 1, inputDisks[diskIndex]) == 1; i++) {
         heap_insert(heap, rd);
         numRecordsRead[diskIndex]++;
@@ -203,9 +204,14 @@ void mergeOrderedBlocks(FILE **inputDisks, FILE *outputDisk, size_t sizeOfElemen
     heap_destroy(heap);
 }
 
-void externalSort(const char *pathFile, unsigned fileSize, size_t sizeOfElements,
-        int (*compare)(const void*, const void*), unsigned internalMemorySize, unsigned pathsNumber) {
-
+void externalSort(const char *pathFile, int fileSize, size_t sizeOfElements,
+        int (*compare)(const void*, const void*), int internalMemorySize, int pathsNumber) {
+    
+    int numberOfPasses = (int) ceil(log((double) fileSize / internalMemorySize) / log((double) pathsNumber));
+    
+    if(numberOfPasses < 0)
+        return;
+    
     FILE *file = fopen(pathFile, "rb+"); // open input file
 
     //create disks
@@ -213,7 +219,7 @@ void externalSort(const char *pathFile, unsigned fileSize, size_t sizeOfElements
     FILE** outputDisks = (FILE**) malloc(sizeof (FILE*) * pathsNumber);
 
     //first stage
-    for (unsigned i = 0; i < pathsNumber; i++) {
+    for (int i = 0; i < pathsNumber; i++) {
         inputDisks[i] = NULL;
         outputDisks[i] = tmpfile();
     }
@@ -221,11 +227,11 @@ void externalSort(const char *pathFile, unsigned fileSize, size_t sizeOfElements
     createOrderedBlocks(file, outputDisks, sizeOfElements, compare, internalMemorySize, pathsNumber);
 
     printf("\n===========================First Stage==============================\n");
-    printf("Tamanho Bloco = %d || Numero de blocos = %d\n", internalMemorySize, (unsigned) ceil(((double) fileSize / internalMemorySize) / pathsNumber));
-    for (unsigned i = 0; i < pathsNumber; i++) {
+    printf("Tamanho Bloco = %d || Numero de blocos = %d\n", internalMemorySize, (int) ceil(((double) fileSize / internalMemorySize) / pathsNumber));
+    for (int i = 0; i < pathsNumber; i++) {
         rewind(outputDisks[i]);
-        unsigned num;
-        printf("disco %u: ", i);
+        int num;
+        printf("disco %d: ", i);
         while (fread(&num, sizeOfElements, 1, outputDisks[i]) == 1)
             printf("%d ", num);
         printf("\n");
@@ -233,40 +239,39 @@ void externalSort(const char *pathFile, unsigned fileSize, size_t sizeOfElements
     printf("====================================================================\n");
 
     //second stage
-    unsigned numberOfPasses = (unsigned) ceil(log((double) fileSize / internalMemorySize) / log((double) pathsNumber));
-    unsigned blockSize = internalMemorySize;
+    int blockSize = internalMemorySize;
 
-    printf("\nNumber of passes %u\n", numberOfPasses);
+    printf("\nNumber of passes %d\n", numberOfPasses);
 
     printf("\n===========================Second Stage==============================\n");
-    for (unsigned i = 0; i < numberOfPasses - 1; i++) {
+    for (int i = 0; i < numberOfPasses - 1; i++) {
         FILE** aux = inputDisks;
         inputDisks = outputDisks;
         outputDisks = aux;
 
-        for (unsigned i = 0; i < pathsNumber; i++) {
+        for (int i = 0; i < pathsNumber; i++) {
             rewind(inputDisks[i]);
             if (outputDisks[i] != NULL)
                 fclose(outputDisks[i]);
             outputDisks[i] = tmpfile();
         }
 
-        unsigned diskIndex;
-        unsigned blocksNumber = (unsigned) ceil(((double) fileSize / blockSize) / pathsNumber);
-        for (unsigned i = 0; i < blocksNumber; i++) {
+        int diskIndex;
+        int blocksNumber = (int) ceil(((double) fileSize / blockSize) / pathsNumber);
+        for (int i = 0; i < blocksNumber; i++) {
             diskIndex = i % pathsNumber;
             mergeOrderedBlocks(inputDisks, outputDisks[diskIndex], sizeOfElements, compare,
                     internalMemorySize, pathsNumber, blockSize);
         }
         
-        blockSize = pow(blockSize, 2);
+        blockSize *= pathsNumber;
         
         //============================================================================
-        printf("Passo: %d || Tamanho Bloco = %d || Numero de blocos = %d\n", i, blockSize, (unsigned) ceil(((double) fileSize / blockSize) / pathsNumber));
-        for (unsigned i = 0; i < pathsNumber; i++) {
+        printf("Passo: %d || Tamanho Bloco = %d || Numero de blocos = %d\n", i, blockSize, (int) ceil(((double) fileSize / blockSize) / pathsNumber));
+        for (int i = 0; i < pathsNumber; i++) {
             rewind(outputDisks[i]);
-            unsigned num;
-            printf("disco %u: ", i);
+            int num;
+            printf("disco %d: ", i);
             while (fread(&num, sizeOfElements, 1, outputDisks[i]) == 1)
                 printf("%d ", num);
             printf("\n");
@@ -274,15 +279,15 @@ void externalSort(const char *pathFile, unsigned fileSize, size_t sizeOfElements
         printf("====================================================================\n");
     }
 
-    for (unsigned i = 0; i < pathsNumber; i++)
+    for (int i = 0; i < pathsNumber; i++)
         rewind(outputDisks[i]);
     rewind(file);
 
-    for (unsigned i = 0; i < pathsNumber; i++)
+    for (int i = 0; i < pathsNumber; i++)
         mergeOrderedBlocks(outputDisks, file, sizeOfElements, compare,
             internalMemorySize, pathsNumber, blockSize);
 
-    for (unsigned i = 0; i < pathsNumber; i++) {
+    for (int i = 0; i < pathsNumber; i++) {
         fclose(inputDisks[i]);
         fclose(outputDisks[i]);
     }
